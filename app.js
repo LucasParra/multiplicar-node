d2e4fb88a7801a7264b92308ac3a71b0eefe4e3c
lparra98 <l.parra03@ufromail.cl>
2018-07-17 15:07:37 -0400

    Comit inicial

---
+++ app.js
@@ -0,0 +1,24 @@
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
+const argv = require('./config/yargs').argv;
+const colors = require('colors/safe');
+
+let comando = argv._[0];
+switch (comando)
+{
+    case 'listar':
+        listarTabla(argv.base,argv.limite);
+    break;
+    case 'crear':
+        crearArchivo(argv.base,argv.limite).then((archivo) =>
+        {
+            console.log(`Archivo creado tabla : ` + colors.green(archivo));
+        }).catch((err)=>
+        {
+            console.log(err);
+        });
+    break;
+    default:
+        console.log('Comando no reconocido');
+        break;
+}
+let parametro = argv[2];
